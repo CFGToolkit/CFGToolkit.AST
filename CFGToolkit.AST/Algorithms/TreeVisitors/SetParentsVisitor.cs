badbaa73c17adbaafc0fd798839b704caeb84d3b
Marcin Golebiowski <xormus@gmail.com>
2022-01-31 18:42:54 +0100

    Change to .NET 6.0 and add SetParentsVisitor class

---
+++ CFGToolkit.AST/Algorithms/TreeVisitors/SetParentsVisitor.cs
@@ -0,0 +1,42 @@
+﻿namespace CFGToolkit.AST.Algorithms.TreeVisitors
+{
+    public class SetParentsVisitor : TreeVistorBase
+    {
+        public override void Visit(SyntaxNode node)
+        {
+            if (node?.Children != null)
+            {
+                foreach (var child in node.Children)
+                {
+                    child.Parent = node;
+                    Visit(child);
+                }
+            }
+        }
+
+        public override void Visit(SyntaxNodeMany many)
+        {
+            if (many?.Repeated != null)
+            {
+                foreach (var child in many.Repeated)
+                {
+                    child.Parent = many;
+                    Visit(child);
+                }
+            }
+        }
+
+        public override void Visit(SyntaxNodeOption option)
+        {
+            if (option?.Inside != null)
+            {
+                option.Inside.Parent = option;
+                Visit(option.Inside);
+            }
+        }
+
+        public override void Visit(SyntaxToken token)
+        {
+        }
+    }
+}
